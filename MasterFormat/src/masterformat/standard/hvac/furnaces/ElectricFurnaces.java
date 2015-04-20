2092e0bc5eeef1e69bef58ca278cee4fbf41fc44
weilixu <weilix@andrew.cmu.edu>
2015-04-20 17:04:30 -0400

    move furnace data to sql

--- MasterFormat/src/masterformat/standard/hvac/furnaces/ElectricFurnaces.java
+++ MasterFormat/src/masterformat/standard/hvac/furnaces/ElectricFurnaces.java
@@ -1,6 +1,7 @@
 package masterformat.standard.hvac.furnaces;
 
-import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -8,9 +9,7 @@ import java.util.Set;
 public class ElectricFurnaces extends AbstractFurnace {
 
     private Double power;
-    
-    private final Double[] powerList = {9999.3};
-    
+        
     public ElectricFurnaces(){
 	unit = "$/Ea.";
 	hierarchy = "235400 Furnaces:235413 Electric-Resistance Furnaces:235413.10 Electric Furnaces";
@@ -39,81 +38,56 @@ public class ElectricFurnaces extends AbstractFurnace {
 
     @Override
     protected void initializeData() {
-	Double[][] costsMatrix = {{455.0,233.0,0.0,688.0,855.0}};
-	
-	optionLists = new ArrayList<String>();
-	optionQuantities = new ArrayList<Integer>();
-	optionLists.add("Electric Furnaces, Hot air, blowers, std.controls not including gas, oil or flue piping, 10,000 watts");
-	optionQuantities.add(0);
-	
-	for (int i = 0; i < optionLists.size(); i++) {
-	    priceData.put(optionLists.get(i), costsMatrix[i]);
-	}
+	//nothing to initialize
     }
     
     @Override
     public void selectCostVector() {
-	setToZero();
-	if(power<=9993.7){
-	    description = optionLists.get(0);
-	    costVector = deepCopyCost(priceData.get(description));
-	    Integer i = optionQuantities.get(0);
-	    optionQuantities.set(0, i+1);
-	}else{
-	    description = "Electric Furnaces, Hot air, blowers, std.controls not including gas, oil or flue piping, grouped";
-	    fittingPower();
-	}
-    }
-    
-    private void fittingPower(){
-	//shows the best fit capacity
-	Double fittedPower=0.0;
-	//shows the total capacity added
-	Double totalPower=0.0;
-	costVector=deepCopyCost(Default_Cost_Vector);
-	
-	while(totalPower<power){
-	    fittedPower = findFittedPower(totalPower);
-	    totalPower+=fittedPower;
-	}
-    }
-    
-    private Double findFittedPower(Double total){
-	//the difference between capacity and total capacity
-	Double temp = power;
-	//index shows the current best fit capacity
-	int criticalIndex = 0;
-	
-	for(int i=0; i<powerList.length; i++){
-	    Double residual = Math.abs(power-total-powerList[i]);
-	    if(residual<temp){
-		temp = residual;
-		criticalIndex = i;
+	Double[] cost = new Double[numOfCostElement];
+	try {
+	    connect = DriverManager
+		    .getConnection("jdbc:mysql://localhost/hvac?"
+			    + "user=root&password=911383");
+	    statement = connect.createStatement();
+
+	    int numberOfFurnace = 1;
+
+	    resultSet = statement
+		    .executeQuery("select * from hvac.furnaces where source = electric and power>='"
+			    + power + "'");
+
+	    if (!resultSet.next()) {
+		// this means there is no such furnace that can satisfy the
+		// capacity. We need to modularize the furnace
+		double furnaceCapacity = power;
+		while (!resultSet.next()) {
+		    numberOfFurnace += 1;
+		    furnaceCapacity = furnaceCapacity / 2;
+		    resultSet = statement
+			    .executeQuery("select * from hvac.furnaces where source = electric and power>='"
+				    + furnaceCapacity + "'");
+		}
+		cost[materialIndex] = resultSet.getDouble("materialcost")
+			* numberOfFurnace;
+		cost[laborIndex] = resultSet.getDouble("laborcost")
+			* numberOfFurnace;
+		cost[equipIndex] = resultSet.getDouble("equipmentcost")
+			* numberOfFurnace;
+		cost[totalIndex] = resultSet.getDouble("totalCost")
+			* numberOfFurnace;
+		cost[totalOPIndex] = resultSet.getDouble("totalInclop")
+			* numberOfFurnace;
+
+		description = resultSet.getString("description");
+		costVector = cost;
+
+		optionLists.add(description);
+		optionQuantities.add(numberOfFurnace);
 	    }
-	}
-	//add to the cost vector
-	Double[] itemCost = priceData.get(optionLists.get(criticalIndex));
-	for(int j=0; j<costVector.length; j++){
-	    costVector[j]+=itemCost[j];
-	}
-	Integer q = optionQuantities.get(criticalIndex)+1;
-	optionQuantities.set(criticalIndex, q);
-	
-	return powerList[criticalIndex];
-    }
-    
-    private void setToZero(){
-	for(int i=0; i<optionQuantities.size(); i++){
-	    optionQuantities.set(i, 0);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    close();
 	}
     }
-
-    private Double[] deepCopyCost(Double[] costVector){
-	Double[] temp = new Double[costVector.length];
-	for(int i=0; i<costVector.length; i++){
-	    temp[i]= costVector[i];
-	}
-	return temp;
-    }
-
 }
--- MasterFormat/src/masterformat/standard/hvac/furnaces/FuelFiredFurnaces.java
+++ MasterFormat/src/masterformat/standard/hvac/furnaces/FuelFiredFurnaces.java
@@ -1,17 +1,17 @@
 package masterformat.standard.hvac.furnaces;
 
-import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class FuelFiredFurnaces extends AbstractFurnace{
-    
+public class FuelFiredFurnaces extends AbstractFurnace {
+
     private Double power;
-    
-    private final Double[] powerList = {13188.2,17584.3,21980.3,29307.1};
-    
-    public FuelFiredFurnaces(){
+
+
+    public FuelFiredFurnaces() {
 	unit = "$/Ea.";
 	hierarchy = "235400 Furnaces:235416 Fuel-Fired Furnaces:235416.13 Gas-Fired Furnaces";
     }
@@ -30,109 +30,65 @@ public class FuelFiredFurnaces extends AbstractFurnace{
 
     @Override
     public void setVariable(String[] surfaceProperties) {
-	try{
+	try {
 	    power = Double.parseDouble(surfaceProperties[powerIndex]);
-	}catch(NumberFormatException e){
+	} catch (NumberFormatException e) {
 	    userInputs.add("INPUT:Power:Watt");
-	}	
+	}
     }
 
     @Override
     protected void initializeData() {
-	Double[][] costsMatrix = {{535.0,201.0,0.0,736.0,900.0},
-		{535.0,212.0,0.0,747.0,915.0},
-		{575.0,224.0,0.0,799.0,975.0},
-		{625.0,252.0,0.0,877.0,1075.0}
-	};
-	
-	optionLists = new ArrayList<String>();
-	optionQuantities = new ArrayList<Integer>();
-	optionLists.add("Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 13188 watts");
-	optionQuantities.add(0);
-	optionLists.add("Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 17584 watts");
-	optionQuantities.add(0);
-	optionLists.add("Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 21980 watts");
-	optionQuantities.add(0);
-	optionLists.add("Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 29307 watts");
-	optionQuantities.add(0);
-	
-	for (int i = 0; i < optionLists.size(); i++) {
-	    priceData.put(optionLists.get(i), costsMatrix[i]);
-	}
+	//nothing to initialize
     }
-    
+
     @Override
     public void selectCostVector() {
-	if(power<=13188.2){
-	    description = "Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 13188 watts";
-	    costVector = deepCopyCost(priceData.get(description));    
-	}else if(power>13188.2 && power<=17584.3){
-	    description = "Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 17584 watts";
-	    costVector = deepCopyCost(priceData.get(description));  
-	}else if(power>17584.3 && power<=21980.3){
-	    description = "Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 21980 watts";
-	    costVector = deepCopyCost(priceData.get(description));  
-	}else if(power>21980.3 && power<=29307.1){
-	    description = "Gas-Fired Furnaces, AGA certified, upflow, direct drive models, 29307 watts";
-	    costVector = deepCopyCost(priceData.get(description)); 
-	}else{
-	    description = "Gas-Fired Furnaces, AGA certified, upflow, direct drive models, grouped";
-	    fittingPower();
-	}
-    }
-    
-    private void fittingPower(){
-	setToZero();
-	//shows the best fit capacity
-	Double fittedPower=0.0;
-	//shows the total capacity added
-	Double totalPower=0.0;
-	costVector=deepCopyCost(Default_Cost_Vector);
-	
-	while(totalPower<power){
-	    fittedPower = findFittedPower(totalPower);
-	    totalPower+=fittedPower;
-	}
-    }
-    
-    private Double findFittedPower(Double total){
-	//the difference between capacity and total capacity
-	Double temp = power;
-	//index shows the current best fit capacity
-	int criticalIndex = 0;
-	
-	for(int i=0; i<powerList.length; i++){
-	    Double residual = Math.abs(power-total-powerList[i]);
-	    if(residual<temp){
-		temp = residual;
-		criticalIndex = i;
+	Double[] cost = new Double[numOfCostElement];
+	try {
+	    connect = DriverManager
+		    .getConnection("jdbc:mysql://localhost/hvac?"
+			    + "user=root&password=911383");
+	    statement = connect.createStatement();
+
+	    int numberOfFurnace = 1;
+
+	    resultSet = statement
+		    .executeQuery("select * from hvac.furnaces where source = gas and power>='"
+			    + power + "'");
+
+	    if (!resultSet.next()) {
+		// this means there is no such furnace that can satisfy the
+		// capacity. We need to modularize the furnace
+		double furnaceCapacity = power;
+		while (!resultSet.next()) {
+		    numberOfFurnace += 1;
+		    furnaceCapacity = furnaceCapacity / 2;
+		    resultSet = statement
+			    .executeQuery("select * from hvac.furnaces where source = gas and power>='"
+				    + furnaceCapacity + "'");
+		}
+		cost[materialIndex] = resultSet.getDouble("materialcost")
+			* numberOfFurnace;
+		cost[laborIndex] = resultSet.getDouble("laborcost")
+			* numberOfFurnace;
+		cost[equipIndex] = resultSet.getDouble("equipmentcost")
+			* numberOfFurnace;
+		cost[totalIndex] = resultSet.getDouble("totalCost")
+			* numberOfFurnace;
+		cost[totalOPIndex] = resultSet.getDouble("totalInclop")
+			* numberOfFurnace;
+
+		description = resultSet.getString("description");
+		costVector = cost;
+
+		optionLists.add(description);
+		optionQuantities.add(numberOfFurnace);
 	    }
-	}
-	
-	//add to the cost vector
-	Double[] itemCost = priceData.get(optionLists.get(criticalIndex));
-	for(int j=0; j<costVector.length; j++){
-	    costVector[j]+=itemCost[j];
-	}
-	Integer q = optionQuantities.get(criticalIndex)+1;
-	optionQuantities.set(criticalIndex, q);
-	
-	return powerList[criticalIndex];
-    }
-    
-    private void setToZero(){
-	for(int i=0; i<optionQuantities.size(); i++){
-	    optionQuantities.set(i, 0);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    close();
 	}
     }
-
-    private Double[] deepCopyCost(Double[] costVector){
-	Double[] temp = new Double[costVector.length];
-	for(int i=0; i<costVector.length; i++){
-	    temp[i]= costVector[i];
-	}
-	return temp;
-    }
-
-
 }
